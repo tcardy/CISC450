bec7dd2191b70123b5608518ae22f958feb6de05
tcardy <thomasb.cardy@gmail.com>
2017-04-01 16:47:21 -0400

    bankserver.cpp v1.1
    Tested and improved functionality of the server side

--- Project 1/Server/bankserver.cpp
+++ Project 1/Server/bankserver.cpp
@@ -74,59 +74,6 @@ Account checking;
 Account savings;
 
 // Global communication variables
-int sock_server;  /* Socket on which server listens to clients */
-int sock_connection;  /* Socket on which server exchanges data with client */
-struct sockaddr_in server_addr;  /* Internet address structure that stores server address */
-unsigned int server_addr_len;  /* Length of server address structure */
-unsigned short server_port;  /* Port number used by server (local port) */
-struct sockaddr_in client_addr;  /* Internet address structure that stores client address */
-unsigned int client_addr_len;  /* Length of client address structure */
-char sentence[STRING_SIZE];  /* receive message */
-char modifiedSentence[STRING_SIZE]; /* send message */
-unsigned int msg_len;  /* length of message */
-int bytes_sent, bytes_recd; /* number of bytes sent or received */
-unsigned int i;  /* temporary loop variable */
-
-/* open a socket */
-void open_socket(){
-	if ((sock_server = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-      perror("Server: can't open stream socket");
-      exit(1);                                                
-   }
-}
-
-/* initialize server address information */
-void init_server_addr(){
-   memset(&server_addr, 0, sizeof(server_addr));
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = htonl (INADDR_ANY);  /* This allows choice of
-                                        any host interface, if more than one
-                                        are present */ 
-   server_port = SERV_TCP_PORT; /* Server will listen on this port */
-   server_addr.sin_port = htons(server_port);
-}
-
-/* bind the socket to the local server port */
-void bind_socket(){
-   if (bind(sock_server, (struct sockaddr *) &server_addr,
-                                    sizeof (server_addr)) < 0) {
-      perror("Server: can't bind to local address");
-      close(sock_server);
-      exit(1);
-   }         
-}
-
-/* listen for incoming requests from clients */
-void listen_for_reqs(){
-   if (listen(sock_server, 50) < 0) {    /* 50 is the max number of pending */
-      perror("Server: error on listen"); /* requests that will be queued */
-      close(sock_server);
-      exit(1);
-   }
-   printf("I am here to listen ... on port %hu\n\n", server_port);
-  
-   client_addr_len = sizeof (client_addr);
-}
 
 void change_amount_sentence(char sentence[], int amount, int index){
 	bitset<20> b1 (amount);
@@ -234,6 +181,8 @@ void process_transfer(char sentence[]){
 
 void process_transaction(char sentence[]){
 	
+	cout << "Processing transaction\n" << endl;
+	
 	if (sentence[1] == '0')
 		process_check_balance(sentence);
 	else if (sentence[1] == '1')
@@ -250,23 +199,70 @@ int main(void){
 	init_account(checking, 1, 'c', 0);
 	init_account(savings, 2, 's',0);
 	
-	//open_socket();
-	//init_server_addr();
-	//bind_socket();
-	//listen_for_reqs();
+	int sock_server;  /* Socket on which server listens to clients */
+	int sock_connection;  /* Socket on which server exchanges data with client */
+	struct sockaddr_in server_addr;  /* Internet address structure that stores server address */
+	unsigned int server_addr_len;  /* Length of server address structure */
+	unsigned short server_port;  /* Port number used by server (local port) */
+	struct sockaddr_in client_addr;  /* Internet address structure that stores client address */
+	unsigned int client_addr_len;  /* Length of client address structure */
+	char sentence[STRING_SIZE];  /* receive message */
+	char modifiedSentence[STRING_SIZE]; /* send message */
+	unsigned int msg_len;  /* length of message */
+	int bytes_sent, bytes_recd; /* number of bytes sent or received */
 	
-	/*
+	/* open a socket */
+
+   if ((sock_server = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+      perror("Server: can't open stream socket");
+      exit(1);                                                
+   }
+
+   /* initialize server address information */
+    
+   memset(&server_addr, 0, sizeof(server_addr));
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_addr.s_addr = htonl (INADDR_ANY);  /* This allows choice of
+                                        any host interface, if more than one
+                                        are present */ 
+   server_port = SERV_TCP_PORT; /* Server will listen on this port */
+   server_addr.sin_port = htons(server_port);
+
+   /* bind the socket to the local server port */
+
+   if (bind(sock_server, (struct sockaddr *) &server_addr,
+                                    sizeof (server_addr)) < 0) {
+      perror("Server: can't bind to local address");
+      close(sock_server);
+      exit(1);
+   }                     
+
+   /* listen for incoming requests from clients */
+
+   if (listen(sock_server, 50) < 0) {    /* 50 is the max number of pending */
+      perror("Server: error on listen"); /* requests that will be queued */
+      close(sock_server);
+      exit(1);
+   }
+   printf("I am here to listen ... on port %hu\n\n", server_port);
+  
+   client_addr_len = sizeof (client_addr);
+
+   /* wait for incoming connection requests in an indefinite loop */
+   
+   sock_connection = accept(sock_server, (struct sockaddr *) &client_addr, 
+                                         &client_addr_len);
+                     /* The accept function blocks the server until a
+                        connection request comes from a client */
+    if (sock_connection < 0) {
+       perror("Server: accept() error\n"); 
+       close(sock_server);
+       exit(1);
+    }
+	
 	while(1){
-		sock_connection = accept(sock_server, (struct sockaddr *) &client_addr, 
-            &client_addr_len); /* The accept function blocks the server until a
-                connection request comes from a client 
-		if (sock_connection < 0) {
-			perror("Server: accept() error\n"); 
-			close(sock_server);
-			exit(1);
-		}
  
-      /* receive the message 
+      /* receive the message */
 
       bytes_recd = recv(sock_connection, sentence, STRING_SIZE, 0);
 
@@ -275,16 +271,17 @@ int main(void){
          printf("%s", sentence);
          printf("\nwith length %d\n\n", bytes_recd);
 
-        /* prepare the message to send 
+        /* prepare the message to send */
 
 		 process_transaction(sentence);
 		 msg_len = strlen(sentence);
 
-         /* send message 
+         /* send message */
  
          bytes_sent = send(sock_connection, sentence, msg_len, 0);
+		}
 	}
 	close(sock_connection);
-	*/
 	return 0;
+	
 }
