12a7f8584de488c378b22baa0d53955a13c9c544
tcardy <thomasb.cardy@gmail.com>
2017-05-03 13:11:31 -0400

    added comments to send

--- Project 2/send/send.cpp
+++ Project 2/send/send.cpp
@@ -23,7 +23,7 @@
 
 #define STRING_SIZE 1024
 
-FILE * fileIn;
+FILE * fileIn;   
 size_t bytesRead;
 int fileIsOpen;
 int packetNum;
@@ -74,8 +74,8 @@ void open_socket(){
    }
 }
 
+/* clear client address structure and initialize with client address */
 void initialize_address(){
-	/* clear client address structure and initialize with client address */
 	memset(&client_addr, 0, sizeof(client_addr));
 	client_addr.sin_family = AF_INET;
 	client_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* This allows choice of
@@ -84,6 +84,7 @@ void initialize_address(){
 	client_addr.sin_port = htons(client_port);
 }
 
+// Binds the socket
 void bind_socket(){
 	
 	if (bind(sock_client, (struct sockaddr *) &client_addr,
@@ -98,6 +99,7 @@ void bind_socket(){
    fcntl(sock_client, F_SETFL, fcntl_flags | O_NONBLOCK);
 }
 
+// Initializes the server
 void initialize_server(){
 	/*
 	printf("Enter hostname of server: ");
@@ -124,15 +126,17 @@ void initialize_server(){
 	
 }
 
+// Signal handler for timeout events
 void sigalrm_handler(int sig)
 {
     timeExpired = 1;
 	timeoutCount++;
 	timeouts++;
 	cout << "Timeout expired for packet numbered " << seqNum <<endl;
-	signal(SIGALRM, sigalrm_handler); 
+	signal(SIGALRM, sigalrm_handler);  // Ready the handler for another signal
 }
 
+// Sets the timeout timeout
 void set_timer(){
 	timer.it_interval.tv_sec = 0;
 	timer.it_interval.tv_usec = 0;
@@ -142,6 +146,7 @@ void set_timer(){
 	timeExpired = 0;
 }
 
+// Stops the timer when ACK is recv'd 
 void stop_timer(){
 	timer.it_interval.tv_sec = 0;
 	timer.it_interval.tv_usec = 0;
@@ -151,7 +156,7 @@ void stop_timer(){
 	timeExpired = 0;
 }
 
-
+// Initializes global variables
 void initialize_variables(){
 	
 	packetNum = 0;
@@ -166,6 +171,7 @@ void initialize_variables(){
 	timeouts = 0;
 }
 
+// Opens the input file
 int open_file(char filename[]){
 	fileIn = fopen(filename, "rb");
 	if (fileIn == NULL){
@@ -175,10 +181,12 @@ int open_file(char filename[]){
 	return 0;
 }
 
+// closes input
 int close_file(){
 	fclose(fileIn);
 }
 
+// Simulates calls from upper level by reading from file
 int wait_for_call_from_above(char buffer[]){
 	
 	if (fgets (buffer , 80 , fileIn) == NULL){
@@ -191,6 +199,7 @@ int wait_for_call_from_above(char buffer[]){
 	return bytesRead;
 }
 
+// Examines the ACK recv'd
 int read_ack(char ack_packet[]){
 	
 	uint16_t ack;
@@ -207,6 +216,8 @@ int read_ack(char ack_packet[]){
 		set_timer();
 	}
 }
+
+// Read ACK from buffer
 void recv_ack(){
 	
 	bytes_recd = recvfrom(sock_client, ack_packet, 1024, 0,
@@ -217,6 +228,7 @@ void recv_ack(){
 	}
 }
 
+// Sends a packet to the receiver
 int send_packet(char packet[]){
 	
 	msg_len = dataBytesSent + 5;
@@ -226,6 +238,7 @@ int send_packet(char packet[]){
 	totaldataBytesSent += (bytes_sent - 5);
 }
 
+// Resends the data packet due to timeout
 int resend_packet(char packet[]){
 	msg_len = dataBytesSent + 5;
 	bytes_sent = sendto(sock_client, packet, msg_len, 0,
@@ -234,6 +247,7 @@ int resend_packet(char packet[]){
 	numOfResentPackets++;
 }
 
+// Readies the packet for sending
 int make_packet(char buffer[]){
 	
 	dataBytesSent = strlen(buffer) + 1;
@@ -244,6 +258,7 @@ int make_packet(char buffer[]){
 	memcpy(packet + 4, buffer, dataBytesSent);
 }
 
+// Transmits the EOT packet
 void send_eot(){
 	
 	uint16_t eotCount = htons((uint16_t)0);
@@ -256,12 +271,14 @@ void send_eot(){
 	cout << seqNum << " transmitted with 0 data bytes" << endl;
 }
 
+// Calculates the timeout value based upon user's input
 void determine_timeout_values(){
 	timeout_value = pow(10, exponent);
 	timeout_secs = (timeout_value / 1000000);
 	timeout_usecs = (timeout_value - (timeout_secs * 1000000));
 }
 
+// Waits until ACK is recv'd or timeout
 void wait_for_ack(){
 	
 	while(1){
@@ -276,6 +293,8 @@ void wait_for_ack(){
 	}
 }
 
+/* Clears the recv buffer in case of duplicate ACK from duplicate 
+	transmissions */
 void clear_recv_buffer(){
 	int bytesAv = 0;
 	
@@ -291,6 +310,7 @@ void clear_recv_buffer(){
 	bytes_recd = 0;
 }
 
+// Resets critical values for next iteration
 void next_iteration(){
 	
 	timeExpired = 0;
@@ -300,6 +320,7 @@ void next_iteration(){
 	packetNum++;
 }
 
+// Prints the EOT report
 void print_report(){
 	
 	cout << "Number of data packets transmitted: " << packetNum << endl;
@@ -311,6 +332,7 @@ void print_report(){
 	cout << "Time, in milliseconds, taken to transmit file: " << duration << endl;
 }
 
+// Converts secs and usecs to msecs
 void calculate_duration(){
 	duration = ((end.tv_sec * 1000 + end.tv_usec / 1000)
 		  - (start.tv_sec * 1000 + start.tv_usec / 1000));
@@ -326,11 +348,12 @@ int main(void){
 	char buffer[80];
 	char filename[1024];
 	initialize_variables();
-	signal(SIGALRM, sigalrm_handler); 
+	signal(SIGALRM, sigalrm_handler); // Readies the signal handler for first signal
 
 	cout << "Please enter a timeout exponent (Between 1 and 10):\n" << endl;
 	cin >> exponent;
 	
+	// Loops until user inputs a proper value for timeout exponent
 	while (cin.fail() || exponent > 10 || exponent < 1){
 		cout << "Please enter a number between 1 and 10\n" << endl;
 		cin.clear(); 
@@ -339,6 +362,7 @@ int main(void){
 	}
 	determine_timeout_values();
 	
+	// Loops until user enters a valid path to input file
 	do{
 		cout << "\nPlease enter a file name (must include either absolute or relative path)" << endl;
 		cin >> filename;
@@ -346,9 +370,9 @@ int main(void){
 		fileIsOpen = open_file(filename);
 	} while (fileIsOpen < 0);
 	
-	
-	fileIsOpen = open_file(filename);
+	// Starts the duration timer
 	gettimeofday(&start, NULL);
+	// Main loop: sends packets to recv program until input file ends
 	do{		
 		wait_for_call_from_above(buffer);
 		make_packet(buffer);
@@ -370,6 +394,8 @@ int main(void){
 		
 		
 	} while (bytesRead > 0);
+	
+	// Final cleanup and reporting
 	send_eot();
 	gettimeofday(&end, NULL);
 	calculate_duration();
